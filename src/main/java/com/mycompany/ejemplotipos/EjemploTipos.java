55e9e18abcda077d93d2bb91d51bf26d58f372ef
jrfortes <jose.lopez.fortes@hotmail.es>
2022-03-29 18:53:12 +0200

    Subir proyecto tipo de datos

---
+++ src/main/java/com/mycompany/ejemplotipos/EjemploTipos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejemplotipos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EjemploTipos {
+    public static void main(String[] args) {
+        int i=10;
+        double d=3.14;
+        char c1='a';
+        boolean encontrado=true;
+        String msj="\"Bienvenido a Java\"";
+            
+        System.out.println("La variable i es de tipo entero y su valor es: "+i);
+        System.out.println("La variable d es de tipo entero y su valor es: "+d);
+        System.out.println("La variable c1 es de tipo entero y su valor es: "+c1);
+        System.out.println("La variable encontrado es de tipo entero y su valor es: "+encontrado);
+        System.out.println("La variable msj es de tipo entero y su valor es: "+msj);
+        
+    }    
+}
